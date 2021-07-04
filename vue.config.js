4b4331dbb402724f2857bf41f82e8b1f1fdb30f9
jadebe <myjade999@gmail.com>
2021-07-04 13:25:25 +0900

    경로수정2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : './'
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://myjadepage.github.io/atdge/' : '/'
 };
 //git subtree push --prefix dist origin gh-pages
